6e800a17b2aff553ddfc182e7d96eaf90e94d273
Mahfuza <ibragimovamahfuza464@gmail.com>
2023-08-05 16:42:20 +0500

    Js added

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-// "use strict";
-let javaScript_Name = +prompt("JavaScript nomini kiriting:");
-if (javaScript_Name == "ECMAScript") {
-  console.log("Ofarin siz topdingiz");
+"use strict";
+let help = prompt("Sizga kerakli kishini kiriting ");
+if (help == "Admin") {
+  let password = prompt("Parolni kiriting:");
+  if (password == "Cansel") {
+    console.log("Canseled");
+  } else if ((password = "TheMaster")) {
+    console.log("Welcome!!!");
+  } else {
+    console.log("Parol xato");
+  }
+} else if (help == "Cansel") {
+  console.log("Canseled");
 } else {
-  console.log("Afsuski siz topa olmadingiz.Nomi ECMAScript edi ");
+  console.log("Men sizni tanimayman");
 }
