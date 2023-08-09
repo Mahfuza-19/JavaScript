2e2dba66a4aced4f747246adadd1e69b5fda72fe
Mahfuza <ibragimovamahfuza464@gmail.com>
2023-08-09 22:23:47 +0500

    Added

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 "use strict";
-let help = prompt("Sizga kerakli kishini kiriting ");
-if (help == "Admin") {
-  let password = prompt("Parolni kiriting:");
-  if (password == "Cansel") {
-    console.log("Canseled");
-  } else if ((password = "TheMaster")) {
-    console.log("Welcome!!!");
-  } else {
-    console.log("Parol xato");
-  }
-} else if (help == "Cansel") {
-  console.log("Canseled");
-} else {
-  console.log("Men sizni tanimayman");
+// For loop
+
+// 1-misol
+
+// let number,
+//   sum = 0,
+//   count = 0;
+// for (count; number != 0; count++) {
+//   number = +prompt("Son kiriting");
+//   console.log(number);
+//   sum += number;
+// }
+// console.log(sum / (count - 1));
+
+////////////////Break and countinue////////////////
+// 2-misol
+// Break --> Break bu siklni qattiy to'xtatish
+// for (let i = 1; i < 100; i++) {
+//   if ((i % 2 == 0 && i % 6 != 0)  (i % 3 == 0 && i % 6 != 0)) console.log(i);
+// }
+let number = +prompt("Son keriting:");
+let fact;
+
+for (let i = 1; i < number; i++) {
+  fact *= i - 1;
+  console.log(fact);
 }
